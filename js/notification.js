22d69fdf47eebc90a27edc8935a2c2549d927b8b
SergiHomsm <sergihoms24@gmail.com>
2022-05-03 16:01:09 +0200

    jsnotification

---
+++ js/notification.js
@@ -0,0 +1,17 @@
+export let renderNotification = () => {
+
+    let button = document.querySelector(".product-button");
+
+    button.addEventListener("click", () => {
+        function ShowDiv() {
+            document.getElementById("").style.display = "";
+        }
+        
+        
+    });  
+    
+
+
+
+
+}
